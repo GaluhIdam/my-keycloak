074b146678e015ec7f09d5134b5a228ea7e99f9d
Galuh Idam <50751337+GaluhIdam@users.noreply.github.com>
2023-09-25 19:59:57 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# Build Keycloak Docker image using the Alpine-based Dockerfile
-docker build -t keycloak -f alpine.Dockerfile .
+# Build the Keycloak container
+docker-compose build
 
-# Run Docker Compose to start Keycloak and PostgreSQL containers
+# Start the containers in detached mode
 docker-compose up -d
 
-# Optional: Check container status
-docker-compose ps
+# Wait for Keycloak to be up and running (you can adjust the timeout as needed)
+echo "Waiting for Keycloak to start..."
+until [ "$(docker-compose exec keycloak curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/auth/)" == "200" ]; do
+    sleep 5
+done
+
+# Display a message once Keycloak is ready
+echo "Keycloak is up and running at http://localhost:8080/auth/"
+
+# You can add additional setup steps here if needed
+
+# To stop the containers, you can run: docker-compose down
